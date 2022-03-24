e7abba4ad688e389c0aedab2b65ceadcf40c853b
lingheng123 <88220400+lingheng123@users.noreply.github.com>
2022-03-24 23:12:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 from fstate_generator import *
 from login import login
 
-NEED_BEFORE = True  # 如需补报则置为True，否则False
+NEED_BEFORE = False  # 如需补报则置为True，否则False
 START_DT = dt.datetime(2022, 3, 22)  # 需要补报的起始日期
 RETRY = 5
 RETRY_TIMEOUT = 120
